8e3ff06a41c741247f51775989dc157f0d348e26
Benjamin CHAILLAN <benjamin.chaillan-pro@laposte.net>
2024-03-27 17:11:24 +0100

    algo 6 check

--- algo/6/sumArr.js
+++ algo/6/sumArr.js
@@ -1,6 +1,7 @@
 /*
 Écris une fonction avec deux paramètres. Ces paramètres sont des tableaux contenant des nombres **stockés sous forme de chaînes de caractères**.
-Ta fonction doit renvoyer **un** tableau, où chaque élément est la somme des éléments des deux arguments correspondants (c'est-à-dire : le premier élément du tableau résultat est égal au premier élément du premier paramètre plus le premier élément du deuxième paramètre) .
+Ta fonction doit renvoyer **un** tableau, où chaque élément est la somme des éléments des deux arguments correspondants
+ (c'est-à-dire : le premier élément du tableau résultat est égal au premier élément du premier paramètre plus le premier élément du deuxième paramètre) .
 Remarque : Si un élément est vide, il doit compter pour 0.
 Ex: 
 sumArr( ["1", "2", "3"], ["2", "4", "1"] ) should return ["3", "6", "4"]
@@ -8,9 +9,14 @@ sumArr( ["2", "7", "3"], ["2", "4", "9"] ) should return ["4", "11", "12"]
 sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) should return ["4", "11", "12", "8", "2"]
 sumArr( ["2", "5", "3"], ["2", "4", "9", "5", "5"] ) should return ["4", "9", "12", "5", "5"]
 */
-
 function sumArr(arrayA, arrayB) {
+  newTab = [];
+  for (i = 0; i < arrayA.length || i < arrayB.length; i++) {
+    const numA = parseInt(arrayA[i]) || 0;
+    const numB = parseInt(arrayB[i]) || 0;
+    newTab.push((numA + numB).toString());
+  }
   // Your code here !
+  return newTab;
 }
-
 module.exports = sumArr;
