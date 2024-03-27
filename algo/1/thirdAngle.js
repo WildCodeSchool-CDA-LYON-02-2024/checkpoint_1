9c4072b0cbf9fa7cd34c544c56b8502906f4bc8a
mohamaddayem <u@example.com>
2024-03-27 16:59:21 +0200

    algo1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,9 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  return 180 - (a + b);
 }
+
+console.log(thirdAngle(90, 30)); 
 
 module.exports = thirdAngle;
