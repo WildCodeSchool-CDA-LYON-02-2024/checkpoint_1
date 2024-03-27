77795a3dcec52c52e903498f0ffb756cfc65a7cf
Patrick Hoang <hoang.patrick@outlook.com>
2024-03-27 14:19:02 +0100

    finish algo 1 part 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -8,6 +8,9 @@ thirdAngle(20, 80) doit retourner 80
 
 function thirdAngle(a, b) {
   // Your code here !
+  let total = 180;
+  let lastangle = total - (a+b)
+return lastangle
 }
 
 module.exports = thirdAngle;
