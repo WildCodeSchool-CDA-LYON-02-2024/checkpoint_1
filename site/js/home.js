b7b921cc549055f24082859c2ea4a1207ef22730
mohamaddayem <u@example.com>
2024-03-27 18:40:07 +0200

    avtarphoto

--- site/js/home.js
+++ site/js/home.js
@@ -4,4 +4,19 @@ function handleHelp(item) {
 
   help[item].classList.toggle("help-visible");
   problem[item].classList.toggle("problem-no-visible");
+
+
+
+
+ 
+  
+}
+
+function changeAvatar() {
+  var avatar = document.getElementById('avatar');
+  if (avatar.src.includes('avatar-bis.png')) {
+    avatar.src = 'image/line-icon-for-avtar-vectorpng'; 
+  } else {
+    avatar.src = 'image/avatar-bis.png';
+  }
 }
