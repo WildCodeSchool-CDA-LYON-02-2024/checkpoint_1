ed0d9d6440956a1e848075260902ec6964c1413c
Johana Lavigne <johana.lavigne@gmail.com>
2024-03-27 16:52:09 +0100

    Updates: JS basis step 3 and 4 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,7 +5,6 @@ const avatarId = document.getElementById('avatarImg');
 const avatarImg = "image/avatar.svg";
 const avatarBisImg = "image/avatar-bis.png";
 
-
 avatarId.addEventListener('click', () => {
   avatarId.setAttribute('src', avatarBisImg);
 });
@@ -13,8 +12,13 @@ avatarId.addEventListener('click', () => {
 // Step 2: Change the Matt text on the paragraph with my Name and color in white
 const firstname = document.getElementById('firstname');
 let promptFirstname = prompt("Please type your firstname:");
+const background = document.querySelector('.pink-bg');
 
 if (promptFirstname !== null) {
   firstname.innerText = `${promptFirstname}`;
   firstname.style.color = "white";
+  // Step 3: Replace BG color with #750FF7 with the same prompt and button.
+  background.style.backgroundColor = "#750FF7";
+  background.style.replace('var(--lightWildColor)', '#750FF7');
 }
+
