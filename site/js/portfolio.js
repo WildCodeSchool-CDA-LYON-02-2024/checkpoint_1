d73c30150c9c37de3e6a49c0edabb616ec0d5c44
Johana Lavigne <johana.lavigne@gmail.com>
2024-03-27 16:38:48 +0100

    Updates: JS basis step 2 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,7 +1,20 @@
 'use strict';
 
-// Change the avatar image when I click on it.
-const avatarImg = document.getElementById('avatarImg');
-avatarImg.addEventListener('click', () => {
-  avatarImg.setAttribute('src', 'image/avatar-bis.png');
-})
+// Step 1: Change the avatar image when I click on it.
+const avatarId = document.getElementById('avatarImg');
+const avatarImg = "image/avatar.svg";
+const avatarBisImg = "image/avatar-bis.png";
+
+
+avatarId.addEventListener('click', () => {
+  avatarId.setAttribute('src', avatarBisImg);
+});
+
+// Step 2: Change the Matt text on the paragraph with my Name and color in white
+const firstname = document.getElementById('firstname');
+let promptFirstname = prompt("Please type your firstname:");
+
+if (promptFirstname !== null) {
+  firstname.innerText = `${promptFirstname}`;
+  firstname.style.color = "white";
+}
