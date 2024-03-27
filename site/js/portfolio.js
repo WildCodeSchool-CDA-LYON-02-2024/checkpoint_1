db813d1c5aa3bb94b87fa0a409064d0fbe8fd155
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2024-03-27 16:55:02 +0100

    finish JS step 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,13 +1,18 @@
 function changeAvatar() {
-    let avatar = document.getElementById('avatar');
-    avatar.src = "image/avatar.svg";
+  let avatar = document.getElementById("avatar");
+  avatar.src = "image/avatar.svg";
 }
 
-function modifyText() {
-    let newName = prompt("Enter your name :");
-    if (newName !== null && newName !== "") {
+function modifyContent() {
+  let newName = prompt("Enter your name :");
+  if (newName !== null && newName !== "") {
+    let newColor = prompt("Enter a color :");
+    if (newColor !== null && newColor !== "") {
       let span = document.getElementById("firstname");
+      let contentSection = document.getElementById("content");
       span.textContent = newName;
       span.style.color = "white";
+      contentSection.style.backgroundColor = newColor;
     }
   }
+}
